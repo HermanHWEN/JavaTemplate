922693d0fb2877179524eee6bb5aeda3af0e6837
Administrator <Administrator@Herman>
2017-03-12 20:29:16 +0800

    Java web template

---
+++ src/main/java/org/herman/mvctemplate/controller/Login.java
@@ -0,0 +1,14 @@
+package org.herman.mvctemplate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Login {
+	
+	@RequestMapping(value="/login")
+	public String hello(){
+		return "Hello";
+	}
+	
+}
